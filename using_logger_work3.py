22924bab02c9951f95bcc793a895199aae270cf6
Dmitriy_K <dimon_sdk@mail.ru>
2024-03-18 21:35:40 +0300

    Initial commit

---
+++ using_logger_work3.py
@@ -0,0 +1,35 @@
+from logger_change_work1 import logger
+import types
+
+
+@logger
+def flat_generator(list_of_list):
+    find_item = lambda x: isinstance(x, str) or not isinstance(x, list)
+
+    for item in list_of_list:
+        if find_item(item):
+            yield item
+        else:
+            yield from flat_generator(item)
+
+
+def test_4():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_4()
